6e5466df0bc9ce43f988e420f132b82c2899e36c
Mirza <binyaminbaig99@gmail.com>
2025-05-17 17:06:02 +0530

    BoilerPlate for Identity Reconcilliation Project

---
+++ src/test/java/Identity/reconcilliation/bitespeed/BitespeedApplicationTests.java
@@ -0,0 +1,13 @@
+package Identity.reconcilliation.bitespeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitespeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
